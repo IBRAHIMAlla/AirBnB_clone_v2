3f7612ac11401447c3c1731bff575ceeae85112f
IBRAHIMAlla <brahimalla973@gmail.com>
2024-01-26 12:49:47 -0800

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Flask web application
+"""
+
+from flask import Flask
+moc = Flask(__name__)
+
+
+@moc.route('/', strict_slashes=False)
+def ind():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@moc.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@moc.route('/c/<text>', strict_slashes=False)
+def c_i_s_fun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@moc.route('/python', strict_slashes=False)
+@moc.route('/python/<text>', strict_slashes=False)
+def py_is_cool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@moc.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+if __name__ == '__main__':
+    moc.run(host='0.0.0.0', port='5000')
